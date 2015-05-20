74f97a3cc733f974118c71f6fc7f017ceb1bd8d1
Victor Cassone <victor.cassone@agapered.com>
2015-05-20 18:53:01 -0500

    added config spec to xml

--- library/src/main/java/me/panavtec/drawableview/DrawableView.java
+++ library/src/main/java/me/panavtec/drawableview/DrawableView.java
@@ -2,12 +2,15 @@ package me.panavtec.drawableview;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.RectF;
 import android.os.Build;
 import android.os.Parcelable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
@@ -49,17 +52,20 @@ public class DrawableView extends View
   public DrawableView(Context context, AttributeSet attrs) {
     super(context, attrs);
     init();
+    setAttributeSet(context, attrs);
   }
 
   public DrawableView(Context context, AttributeSet attrs, int defStyleAttr) {
     super(context, attrs, defStyleAttr);
     init();
+    setAttributeSet(context, attrs);
   }
 
   @TargetApi(Build.VERSION_CODES.LOLLIPOP) public DrawableView(Context context, AttributeSet attrs,
       int defStyleAttr, int defStyleRes) {
     super(context, attrs, defStyleAttr, defStyleRes);
     init();
+    setAttributeSet(context, attrs);
   }
 
   private void init() {
@@ -72,6 +78,33 @@ public class DrawableView extends View
     pathDrawer = new PathDrawer();
     canvasDrawer = new CanvasDrawer();
     setOnTouchListener(this);
+  }
+
+  private void setAttributeSet(Context context, AttributeSet attributeSet){
+
+    TypedArray a = context.getTheme().obtainStyledAttributes(
+            attributeSet,
+            R.styleable.DrawableView,
+            0, 0);
+
+    try {
+      DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+      int windowWidth = metrics.widthPixels;
+      int windowHeight = metrics.heightPixels;
+
+      DrawableViewConfig config = new DrawableViewConfig();
+      config.setCanvasHeight(a.getDimensionPixelSize(R.styleable.DrawableView_canvasHeight, windowHeight));
+      config.setCanvasWidth(a.getDimensionPixelSize(R.styleable.DrawableView_canvasWidth, windowWidth));
+      config.setStrokeColor(a.getColor(R.styleable.DrawableView_strokeColor, context.getResources().getColor(android.R.color.black)));
+      config.setShowCanvasBounds(a.getBoolean(R.styleable.DrawableView_showCanvasBounds, true));
+      config.setMinZoom(a.getFloat(R.styleable.DrawableView_minZoom, 1.0f));
+      config.setMaxZoom(a.getFloat(R.styleable.DrawableView_maxZoom, 3.0f));
+      config.setStrokeWidth(a.getFloat(R.styleable.DrawableView_strokeWidth, 20f));
+      setConfig(config);
+
+    } finally {
+      a.recycle();
+    }
   }
 
   public void setConfig(DrawableViewConfig config) {
